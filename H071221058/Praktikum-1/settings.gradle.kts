f35a389e37ee067123c041d1f255221fc84b5c9c
Kyyneko <mahendrakirana284@gmail.com>
2024-03-21 21:08:34 +0800

    H071221058_Tugas Praktikum-1

---
+++ H071221058/Praktikum-1/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Praktikum1"
+include(":app")
+ 
